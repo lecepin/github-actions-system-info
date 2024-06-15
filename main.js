564926f4c0d089c57de494685de09719fdbce49f
lecepin <383810086@qq.com>
2024-06-15 10:48:33 +0800

    feat: disk null

--- main.js
+++ main.js
@@ -1,30 +1,48 @@
 #!/usr/bin/env node
-import si from "systeminformation"
+import si from "systeminformation";
 
-Promise.all([
-  si.osInfo(),
-  si.cpu(),
-  si.mem(),
-  si.memLayout(),
-  si.graphics(),
-  si.diskLayout(),
-]).then(([osInfo, cpu, mem, memLayout, graphics, diskLayout]) => {
-  console.table([
-    {
-      name: "系统信息",
-      info: `${osInfo.distro} ${osInfo.kernel} ${osInfo.arch}`,
-    },
-    {
-      name: "处理器",
-      info: `${cpu.manufacturer} ${cpu.brand} ${cpu.speed}GHz ${cpu.physicalCores}核`,
-    },
-    {
-      name: "内存",
-      info: `${~~(mem.total / Math.pow(1024, 3))}GB`,
-    },
-    {
-      name: "硬盘",
-      info: `${~~(diskLayout[0].size / Math.pow(1024, 3))}GB`,
-    },
-  ]);
+// Promise.all([
+//   si.osInfo(),
+//   si.cpu(),
+//   si.mem(),
+//   si.memLayout(),
+//   si.graphics(),
+//   si.diskLayout(),
+// ]).then(([osInfo, cpu, mem, memLayout, graphics, diskLayout]) => {
+//   console.table([
+//     {
+//       name: "系统信息",
+//       info: `${osInfo.distro} ${osInfo.kernel} ${osInfo.arch}`,
+//     },
+//     {
+//       name: "处理器",
+//       info: `${cpu.manufacturer} ${cpu.brand} ${cpu.speed}GHz ${cpu.physicalCores}核`,
+//     },
+//     {
+//       name: "内存",
+//       info: `${~~(mem.total / Math.pow(1024, 3))}GB`,
+//     },
+//     {
+//       name: "硬盘",
+//       info: `${~~((diskLayout?.[0]?.size ?? 0) / Math.pow(1024, 3))}GB`,
+//     },
+//   ]);
+// });
+
+
+Promise.all([si.osInfo(), si.cpu(), si.mem(), si.memLayout(), si.graphics(), si.diskLayout()]).then(([osInfo, cpu, mem, memLayout, graphics, diskLayout]) => {
+  var _diskLayout$0$size, _diskLayout$;
+  console.table([{
+    name: "系统信息",
+    info: `${osInfo.distro} ${osInfo.kernel} ${osInfo.arch}`
+  }, {
+    name: "处理器",
+    info: `${cpu.manufacturer} ${cpu.brand} ${cpu.speed}GHz ${cpu.physicalCores}核`
+  }, {
+    name: "内存",
+    info: `${~~(mem.total / Math.pow(1024, 3))}GB`
+  }, {
+    name: "硬盘",
+    info: `${~~(((_diskLayout$0$size = diskLayout === null || diskLayout === void 0 || (_diskLayout$ = diskLayout[0]) === null || _diskLayout$ === void 0 ? void 0 : _diskLayout$.size) !== null && _diskLayout$0$size !== void 0 ? _diskLayout$0$size : 0) / Math.pow(1024, 3))}GB`
+  }]);
 });
